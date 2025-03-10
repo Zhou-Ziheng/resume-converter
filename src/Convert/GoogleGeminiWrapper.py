9aee0d5152f35de30cdad08791ff0136f7204cac
Ziheng Zhou <zihengzhou@Zihengs-Laptop.local>
2025-03-10 02:05:58 -0700

    fix gemini model

--- src/Convert/GoogleGeminiWrapper.py
+++ src/Convert/GoogleGeminiWrapper.py
@@ -1,5 +1,3 @@
-
-
 import json
 import logging
 import os
@@ -7,9 +5,10 @@ import time
 import requests
 
 
-URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key="
+URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
 
-class GoogleGenimiWrapper():
+
+class GoogleGenimiWrapper:
     gemini_client = None
 
     @classmethod
@@ -19,7 +18,7 @@ class GoogleGenimiWrapper():
             cls.gemini_client.prompt = open("src/Convert/prompt.txt", "r").read()
             cls.gemini_client.structure = open("src/Convert/structure.json", "r").read()
         return cls.gemini_client
-    
+
     def process_request(self, URL, key, headers, data):
         retries = 3  # Number of retries
         for attempt in range(retries):
@@ -27,10 +26,10 @@ class GoogleGenimiWrapper():
                 # Send POST request
                 response = requests.post(URL + key, headers=headers, json=data)
                 response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
-                data = response.json()['candidates'][0]['content']['parts'][0]['text']
+                data = response.json()["candidates"][0]["content"]["parts"][0]["text"]
                 parsed_data = json.loads(data, strict=False)
                 return parsed_data  # Return parsed data if successful
-            except (requests.RequestException) as e:
+            except requests.RequestException as e:
                 logging.error(f"Error processing request: {e}")
                 logging.error(f"Response: {response.json()['error']}")
                 if attempt < retries - 1:
@@ -51,25 +50,13 @@ class GoogleGenimiWrapper():
         raise Exception("All retries failed.")  # Raise an exception if all retries fail
 
     def format_text(self, text):
-        key = os.environ.get('GEMINI_API_KEY')
+        key = os.environ.get("GEMINI_API_KEY")
         data = {
-        "contents": [
-            {
-                "parts": [
-                    {
-                        "text": self.prompt + self.structure + text
-                    }
-                ]
-            }
-        ]
+            "contents": [{"parts": [{"text": self.prompt + self.structure + text}]}]
         }
 
-        headers = {
-            "Content-Type": "application/json"
-        }
+        headers = {"Content-Type": "application/json"}
 
         parsed_data = self.process_request(URL, key, headers, data)
-        
-        return parsed_data
-    
 
+        return parsed_data
